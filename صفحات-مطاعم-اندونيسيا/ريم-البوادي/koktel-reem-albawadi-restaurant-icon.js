d7c49104afba7b16f7727ee08870e175bab4758d
Bandar-Zuhair <146974861+Bandar-Zuhair@users.noreply.github.com>
2024-06-02 06:14:32 -1200

    Add files via upload

---
+++ صفحات-مطاعم-اندونيسيا/ريم-البوادي/koktel-reem-albawadi-restaurant-icon.js
@@ -0,0 +1,16 @@
+/* in Case If There is Any 'restaurant_orders' Data in The LocalStorage Then Show The 'koktel_order_page_icon' Icon. if No Data Found Then Hide it*/
+if (localStorage.getItem('Reem_Albawadi_LocalStorage')) {
+
+    // Create Button To Show Orders Page
+    let koktel_mealOrderIconDiv = document.createElement('div');
+    koktel_mealOrderIconDiv.id = 'koktel_order_page_icon_div';
+    let koktel_mealOrderIcon = `<a href='https://bandar-zuhair.github.io/Koktel/صفحات-مطاعم-اندونيسيا/ريم-البوادي/طلباتك-من-مطعم-ريم-البوادي.html' id="koktel_order_page_icon"><ion-icon name="cart-outline"></ion-icon></a>`;
+    koktel_mealOrderIconDiv.innerHTML = koktel_mealOrderIcon;
+    document.body.appendChild(koktel_mealOrderIconDiv);
+
+} else {
+    /* if There is No Data in The Orders Key Then Check if The 'koktel_order_page_icon_div' Was Visible Then Hide it */
+    if (document.getElementById('koktel_order_page_icon_div')) {
+        document.getElementById('koktel_order_page_icon_div').style.opacity = '0';
+    }
+}

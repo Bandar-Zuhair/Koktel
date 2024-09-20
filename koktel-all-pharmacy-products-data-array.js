dfc29ab67fc61555197c02b5b4b27cc19d9e6427
Bandar-Zuhair <146974861+Bandar-Zuhair@users.noreply.github.com>
2024-09-20 18:54:03 +0700

    Add files via upload

---
+++ koktel-all-pharmacy-products-data-array.js
@@ -0,0 +1,74 @@
+let heartburnProducts = [
+    {
+        imageSRC: "صيدلية-اندونيسيا/الحموضة-والحرقان/mylanta-obat-maag-cair-50-ml",
+        ar_name: "دواء ميلانتا السائل للحموضة 50 مل",
+        indo_name: "Mylanta Obat Maag Cair 50 Ml",
+        price: "10,000 Rp",
+    },
+];
+
+
+let jointsAndBruisesProducts = [
+    {
+        imageSRC: "صيدلية-اندونيسيا/المفاصل-والكدمات/hot-in-cream-pereda-nyeri-otot-original-120-ml",
+        ar_name: "كريم هوت ان لتخفيف آلام العضلات 120 مل",
+        indo_name: "Hot In Cream Pereda Nyeri Otot Original 120 Ml",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "صيدلية-اندونيسيا/المفاصل-والكدمات/neo-rheumacyl-hot-krim-pereda-nyeri-100-ml",
+        ar_name: "كريم نيو روماسيل لتخفيف آلام العضلات 100 مل",
+        indo_name: "Neo Rheumacyl Hot Krim Pereda Nyeri 100 Ml",
+        price: "10,000 Rp",
+    },
+];
+
+
+let babyPowderProducts = [
+    {
+        imageSRC: "صيدلية-اندونيسيا/بودرة-اطفال/my-baby-bedak-bayi-anak-sweet-floral-235-g",
+        ar_name: "بودرة أطفال ماي بيبي سويت فلورال 235 جم",
+        indo_name: "My Baby Bedak Bayi Anak Sweet Floral 235 G",
+        price: "10,000 Rp",
+    },
+];
+
+
+let honeyProducts = [
+    {
+        imageSRC: "صيدلية-اندونيسيا/عسل/alshifa-madu-murni-jar-250-g",
+        ar_name: "عسل الشفاء الأصلي 250 جم",
+        indo_name: "Alshifa Madu Murni Jar 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "صيدلية-اندونيسيا/عسل/madu-enak-manuka-jar-250-g",
+        ar_name: "عسل مانوكا اللذيذ 250 جم",
+        indo_name: "Madu Enak Manuka Jar 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "صيدلية-اندونيسيا/عسل/tresno-joyo-madu-tj-lemon-botol-150-g",
+        ar_name: "عسل تريسنو جويو بالليمون 150 جم",
+        indo_name: "Tresno Joyo Madu Tj Lemon Botol 150 G",
+        price: "10,000 Rp",
+    },
+];
+
+
+let vicksProducts = [
+    {
+        imageSRC: "صيدلية-اندونيسيا/فكس/cap-lang-minyak-kayu-putih-210-ml",
+        ar_name: "زيت الكالبتوس 210 مل",
+        indo_name: "Cap Lang Minyak Kayu Putih 210 Ml",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "صيدلية-اندونيسيا/فكس/vicks-vaporub-obat-gosok-pereda-flu-original-25-g",
+        ar_name: "فكس للأنفلونزا فابوروب 25 جم",
+        indo_name: "Vicks Vaporub Obat Gosok Pereda Flu Original 25 G",
+        price: "10,000 Rp",
+    },
+];
+
+

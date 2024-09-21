99d39a260d3a3faffdeba8b99bd907cfb144c0bb
Bandar-Zuhair <146974861+Bandar-Zuhair@users.noreply.github.com>
2024-09-21 13:39:17 +0700

    Add files via upload

---
+++ koktel-all-shisha-products-data-array.js
@@ -0,0 +1,185 @@
+let alfakherProducts = [
+    {
+        imageSRC: "معسلات-اندونيسيا/الفاخر/الفاخر-بطيخ-بالنعناع-اشقر",
+        ar_name: "الفاخر بطيخ بالنعاع أشقر 250 جم",
+        indo_name: "Alfakher Semangka Dan Mint Pirang 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/الفاخر/الفاخر-توت-ازرق-اشقر",
+        ar_name: "الفاخر توت ازرق أشقر 250 جم",
+        indo_name: "Alfakher Blueberry Pirang 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/الفاخر/الفاخر-تفاحتين-احمر",
+        ar_name: "الفاخر تفاحتين أحمر 250 جم",
+        indo_name: "Alfakher Dua Apel Merah 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/الفاخر/الفاخر-تفاحتين-اشقر",
+        ar_name: "الفاخر تفاحتين أشقر 250 جم",
+        indo_name: "Alfakher Dua apel Pirang 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/الفاخر/الفاخر-سوبر-ليمون-ونعناع-اشقر",
+        ar_name: "الفاخر سوبر ليمون ونعناع أشقر 250 جم",
+        indo_name: "Alfakher super lemon Dan mint Pirang 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/الفاخر/الفاخر-شمام-اشقر",
+        ar_name: "الفاخر شمام أشقر 250 جم",
+        indo_name: "Alfakher Blewah Pirang 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/الفاخر/الفاخر-عنب-اشقر",
+        ar_name: "الفاخر عنب أشقر 250 جم",
+        indo_name: "Alfakher Anggur pirang 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/الفاخر/الفاخر-عنب-ونعناع-اشقر",
+        ar_name: "الفاخر عنب ونعناع أشقر 250 جم",
+        indo_name: "Alfakher Anggur Dan Mint Pirang 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/الفاخر/الفاخر-عنب-وتوت-اشقر",
+        ar_name: "الفاخر عنب وتوت أشقر 250 جم",
+        indo_name: "Alfakher Anggur Dan Buah Beri pirang 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/الفاخر/الفاخر-ماجيك-لوف-اشقر",
+        ar_name: "الفاخر ماجيك لوف أشقر 250 جم",
+        indo_name: "Alfakher Magic Love 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/الفاخر/الفاخر-ماجيك-لوف-اشقر",
+        ar_name: "الفاخر ماجيك لوف أشقر 250 جم",
+        indo_name: "Alfakher Magic Love Pirang 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/الفاخر/الفاخر-نعناع-اشقر",
+        ar_name: "الفاخر نعناع أشقر 250 جم",
+        indo_name: "Alfakher Mint Pirang 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/الفاخر/الفاخر-وايت-فلاش-اشقر",
+        ar_name: "الفاخر وايت فلاش أشقر 250 جم",
+        indo_name: "Alfakher White Flash Pirang 250 G",
+        price: "10,000 Rp",
+    },
+];
+
+
+let majalisProducts = [
+    {
+        imageSRC: "معسلات-اندونيسيا/مجالس/مجالس-بطيخ-ونعناع",
+        ar_name: "مجالس بطيخ ونعناع 250 جم",
+        indo_name: "Majalis Semangka Dan Mint 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/مجالس/مجالس-توت-ازرق",
+        ar_name: "مجالس توت ازرق 250 جم",
+        indo_name: "Majalis Blueberry 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/مجالس/مجالس-تفاحتين-اشقر",
+        ar_name: "مجالس تفاحتين أشقر 250 جم",
+        indo_name: "Majalis Dua Apel Pirang 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/مجالس/مجالس-عنب-وتوت",
+        ar_name: "مجالس عنب وتوت 250 جم",
+        indo_name: "Majalis Anggur Dan Beri 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/مجالس/مجالس-عنب-ونعناع",
+        ar_name: "مجالس عنب ونعناع 250 جم",
+        indo_name: "Majalis Anggur Dan Mint 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/مجالس/مجالس-فراولة",
+        ar_name: "مجالس فراولة 250 جم",
+        indo_name: "Majalis Stroberi 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/مجالس/مجالس-ليمون-ونعناع",
+        ar_name: "مجالس ليمون ونعناع 250 جم",
+        indo_name: "Majalis Lemon Dan Mint 250 G",
+        price: "10,000 Rp",
+    },
+];
+
+
+let mazayaProducts = [
+    {
+        imageSRC: "معسلات-اندونيسيا/مزايا/مزايا-بطيخ-بالنعناع",
+        ar_name: "مزايا بطيخ بالنعناع 250 جم",
+        indo_name: "Mazaya Semangka Dengan Mint 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/مزايا/مزايا-توت-ازرق-اكوتيكا",
+        ar_name: "مزايا توت ازرق 250 جم",
+        indo_name: "Mazaya Blueberry 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/مزايا/مزايا-توت-بري-مع-نعناع",
+        ar_name: "مزايا توت بري مع نعناع 250 جم",
+        indo_name: "Mazaya Cranberry Dengan Mint 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/مزايا/مزايا-تفاحتين",
+        ar_name: "مزايا تفاحتين 250 جم",
+        indo_name: "Mazaya Dua Apel 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/مزايا/مزايا-نعناع-بالعلكة",
+        ar_name: "مزايا نعناع بالعلكة 250 جم",
+        indo_name: "Mazaya Mint Dengan Permen Karet 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/مزايا/مزايا-نعناع-بالعلكة",
+        ar_name: "مزايا نعناع بالعلكة 250 جم",
+        indo_name: "Mazaya Mint Dengan Permen Karet 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/مزايا/مزايا-فواكه-الجنة",
+        ar_name: "مزايا فواكه الجنة 250 جم",
+        indo_name: "Mazaya Buah Surga 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/مزايا/مزايا-فواكه-الجنة",
+        ar_name: "مزايا فواكه الجنة 250 جم",
+        indo_name: "Mazaya Buah Surga 250 G",
+        price: "10,000 Rp",
+    },
+    {
+        imageSRC: "معسلات-اندونيسيا/مزايا/مزايا-ليمون-و-نعناع",
+        ar_name: "مزايا ليمون ونعناع 250 جم",
+        indo_name: "Mazaya Lemon Dan Mint 250 G",
+        price: "10,000 Rp",
+    },
+];
+

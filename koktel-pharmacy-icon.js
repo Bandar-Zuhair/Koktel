34a49f68c97e14e343b374e1e21bb767edb2ecf0
Bandar-Zuhair <146974861+Bandar-Zuhair@users.noreply.github.com>
2024-10-02 16:40:55 +0700

    Update koktel-pharmacy-icon.js

--- koktel-pharmacy-icon.js
+++ koktel-pharmacy-icon.js
@@ -4,7 +4,7 @@ if (localStorage.getItem('pharmacy_orders')) {
     // Create Button To Show Orders Page
     let koktel_mealOrderIconDiv = document.createElement('div');
     koktel_mealOrderIconDiv.id = 'koktel_order_page_icon_div';
-    let koktel_mealOrderIcon = `<a href='https://bandar-zuhair.github.io/Koktel/طلباتك-من-الصيدلية.html' id="koktel_order_page_icon"><ion-icon name="cart-outline"></ion-icon></a>`;
+    let koktel_mealOrderIcon = `<a href='https://koktel-indo.com/%D8%B7%D9%84%D8%A8%D8%A7%D8%AA%D9%83-%D9%85%D9%86-%D8%A7%D9%84%D8%B5%D9%8A%D8%AF%D9%84%D9%8A%D8%A9' id="koktel_order_page_icon"><ion-icon name="cart-outline"></ion-icon></a>`;
     koktel_mealOrderIconDiv.innerHTML = koktel_mealOrderIcon;
     document.body.appendChild(koktel_mealOrderIconDiv);
 
